8b288ee65512cbc96f23dab27053ff6c47472dad
hssh99 <145898285+hssh99@users.noreply.github.com>
2024-04-06 21:43:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from './search.svg';
 import MovieCard from "./MovieCard";
 
-const API_URL = 'http://www.omdbapi.com?apikey=b61e4dbe';
+const API_URL = 'https://www.omdbapi.com?apikey=b61e4dbe';
 
 function App() {
 
